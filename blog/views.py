d9d534f4bfd392e5cf8792ed5610762fbb117175
moscarellar <moscarellar@gmail.com>
2022-10-23 13:56:45 +0000

    PostForm include in views.py

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 
 
 class PostList(generic.ListView):

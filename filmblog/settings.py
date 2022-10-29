2747e6ac0dcf02c7563e326c706a24a36db74b9b
moscarellar <moscarellar@gmail.com>
2022-10-29 21:25:02 +0000

    ckeditor instalation

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS: 'SAMEORIGIN'
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'ckeditor',
     'django_summernote',
     'crispy_forms',
     'blog',

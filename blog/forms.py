6597d7105a652fa0c637e595872123aa816960d8
moscarellar <moscarellar@gmail.com>
2022-10-30 20:37:06 +0000

    ckeditor content debug for push

--- blog/forms.py
+++ blog/forms.py
@@ -16,12 +16,12 @@ class PostForm (forms.ModelForm):
                   'content', 'author')
 
         widgets = {
-            # 'title': forms.TextInput(attrs={'placeholder':
-            #                                 'Input your title'}),
-            # 'excerpt': forms.TextInput(attrs={'placeholder':
-            #                                   'Write it here'}),
-            # 'content': forms.TextInput(attrs={'placeholder':
-            #                                 'Write your content here'}),
+            'title': forms.TextInput(attrs={'placeholder':
+                                            'Input your title'}),
+            'excerpt': forms.TextInput(attrs={'placeholder':
+                                              'Write it here'}),
+            'content': forms.TextInput(attrs={'placeholder':
+                                            'Write your content here'}),
             'author': forms.TextInput(
                 attrs={'class': 'form-control', 'value': '',
                        'id': 'user', 'type': 'hidden'}),
